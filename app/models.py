fd637d594e5e1b75eba6861e8ccb3d6baed595f8
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-05 01:15:48 +0200

    Add model for cog

--- app/models.py
+++ app/models.py
@@ -1,2 +1,25 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from app.storage import MinioCogStorage
+
+class COG(models.Model):
+    """
+    """
+
+    length = 100
+
+    name = models.CharField(max_length=length, ${blank=False, null=False})
+    image = models.FileField(storage=MinioCogStorage(), ${blank=True, null=True})
+    bucket_name = models.CharField(max_length=length, ${blank=True, null=True})
+    resource_uri = models.URLField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.bucket_name + ":" + self.name
+
+    class Meta:
+        db_table = 'COG'
+        managed = True
+        verbose_name = 'COG'
+        verbose_name_plural = 'COGs'
