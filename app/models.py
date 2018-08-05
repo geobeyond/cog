6589bd13070ca3a7d97af7c049e203424cb6c65f
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-05 15:06:51 +0200

    Comment cog model to minimal needs

--- app/models.py
+++ app/models.py
@@ -10,8 +10,8 @@ class COG(models.Model):
 
     name = models.CharField(max_length=length, blank=False, null=False)
     image = models.FileField(storage=MinioCogStorage(), blank=True, null=True)
-    bucket_name = models.CharField(max_length=length, blank=True, null=True)
-    resource_uri = models.URLField(max_length=500)
+    # bucket_name = models.CharField(max_length=length, blank=True, null=True)
+    # resource_uri = models.URLField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
