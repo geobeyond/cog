46047748e785c556adbdfc5de88c805cda728a27
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-05 12:26:28 +0200

    Fix syntax errors

--- app/models.py
+++ app/models.py
@@ -8,9 +8,9 @@ class COG(models.Model):
 
     length = 100
 
-    name = models.CharField(max_length=length, ${blank=False, null=False})
-    image = models.FileField(storage=MinioCogStorage(), ${blank=True, null=True})
-    bucket_name = models.CharField(max_length=length, ${blank=True, null=True})
+    name = models.CharField(max_length=length, blank=False, null=False)
+    image = models.FileField(storage=MinioCogStorage(), blank=True, null=True)
+    bucket_name = models.CharField(max_length=length, blank=True, null=True)
     resource_uri = models.URLField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
