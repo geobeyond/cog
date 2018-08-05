f399cf6798cc0f75abdf55705f66b24889917429
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-05 15:07:48 +0200

    Add route for api cog view

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from . import views
+from . import views, api_views
 
 urlpatterns = [
-               url(r'^$', views.index, name='index'),
-               url(r'^health', views.health, name='health'),
-               url(r'^404', views.handler404, name='404'),
-               url(r'^500', views.handler500, name='500'),
-               ]
+    url(r'^$', views.index, name='index'),
+    url(r'^api/cogs', api_views.COG_APIView.as_view()),
+    url(r'^health', views.health, name='health'),
+    url(r'^404', views.handler404, name='404'),
+    url(r'^500', views.handler500, name='500'),
+]
