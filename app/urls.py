ef740235bf41fcd0c480fbfd1a7b599951befbaa
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-06 12:33:09 +0200

    Add route name

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,9 @@ from . import views, api_views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(
-        r'^api/cogs',
-        api_views.COG_ListCreateView.as_view()
+        r'^api/cogs/$',
+        api_views.COG_ListCreateView.as_view(),
+        name='cog_create'
     ),
     url(r'^health', views.health, name='health'),
     url(r'^404', views.handler404, name='404'),
