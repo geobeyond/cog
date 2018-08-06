184e151bc57133c6bed7e72059febcd6069ea8ec
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-06 22:58:41 +0200

    Add detail view with get, put, delete methods

--- app/api_views.py
+++ app/api_views.py
@@ -1,6 +1,7 @@
 from .models import COG
 from PIL import Image
 import rasterio
+from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
@@ -60,7 +61,82 @@ class COGListCreateView(APIView):
         cog = COG.objects.create(name=name, image=img)
         cog.save()
         return Response(status=status.HTTP_201_CREATED)
-    
-    # def delete(self, request, format=None):
-    #     COG.delete(save=True)
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class COGDetailView(APIView):
+    """
+    This view should return the Cloud Optimized Geotiff queryset
+    as determined by the uuid portion of the URL.
+    """
+    parser_class = (ImageUploadParser,)
+
+    def get_queryset(self):
+        uuid = self.kwargs['uuid']
+        return COG.objects.filter(id=uuid)
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset)
+        # self.check_object_permissions(self.request, obj)
+        return obj
+
+    def get(self, request, format=None, *args, **kwargs):
+        cog_item = self.get_object()
+        cog = {
+            "id": cog_item.id,
+            "name": cog_item.name,
+            "bucket_name": cog_item.bucket_name,
+            "resource_uri": cog_item.resource_uri,
+            "created_at": cog_item.created_at,
+            "updated_at": cog_item.updated_at
+        }
+        return Response(cog)
+        
+
+    def put(self, request, format=None, *args, **kwargs):
+        """
+
+        Example
+        -------
+        Example of request for creating a COG resource.
+        
+            $   curl -X PUT \
+                http://localhost:5000/api/cogs/<id> \
+                -u 'cog:cog' \
+                -H 'Content-Disposition: attachment; filename=example.tif' \
+                -H 'Content-Type: image/tif' \
+                -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
+                -F image=@/Users/geobart/example.tif \
+                -F name=example.tif
+        """
+        
+        if 'image' not in request.data.keys():
+            raise ParseError("Empty content")
+        
+        img = request.data['image']
+        name = request.data['name']
+
+        # try to open file with rasterio
+        try:
+            with rasterio.open(img) as dataset:
+                data_array = dataset.read() 
+        except:
+            raise ParseError("Unsupported image type opened by Rasterio") 
+
+        cog_item = self.get_object()
+        cog = COG.objects.filter(
+            id=cog_item.id
+        ).update(name=name, image=img)
+        cog.save()
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request, format=None, *args, **kwargs):
+        """
+        """
+
+        cog_item = self.get_object()
+        cog = COG.objects.filter(
+            id=cog_item.id
+        )
+        cog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- app/urls.py
+++ app/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
         api_views.COGListCreateView.as_view(),
         name='cog_create'
     ),
+    url(
+        r'^api/cogs/(?P<uuid>\b[0-9A-Fa-f]{8}\b(-\b[0-9A-Fa-f]{4}\b){3}-\b[0-9A-Fa-f]{12}\b)/$',
+        api_views.COGDetailView.as_view(),
+        name='cog_detail'
+    ),
     url(r'^health', views.health, name='health'),
     url(r'^404', views.handler404, name='404'),
     url(r'^500', views.handler500, name='500'),
