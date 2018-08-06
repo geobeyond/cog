66d288269fb52d4d9e4f4bcbc9bf710c58ad7a13
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2018-08-06 12:21:00 +0200

    Add first working upload

--- app/api_views.py
+++ app/api_views.py
@@ -1,32 +1,58 @@
 from .models import COG
+import rasterio
 from rest_framework.exceptions import ParseError
 from rest_framework.parsers import FileUploadParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+
+
+class ImageUploadParser(FileUploadParser):
+    media_type = 'image/*'
 
 
 class COG_APIView(APIView):
-    parser_class = (FileUploadParser,)
+    parser_class = (ImageUploadParser,)
 
     def get(self, request, format=None):
-        cogs = [cog.name for cog in COG.objects.all()]
+        cogs = [{"name": cog.name} for cog in COG.objects.all()]
         return Response(cogs)
-
-    def post(self, request, format=None):
-        if 'file' not in request.data:
-            raise ParseError("Empty content")
-
-        f = request.data['file']
-
-        COG.image.save(f.name, f, save=True)
-        return Response(status=status.HTTP_201_CREATED)
         
 
-    def put(self, request, format=None):
-        if 'file' not in request.data:
+    def post(self, request, format=None):
+        """
+
+        Example
+        -------
+        Example of request for creating a COG resource.
+        
+            $   curl -X POST \
+                http://localhost:5000/api/cogs/ \
+                -u 'cog:cog' \
+                -H 'Content-Disposition: attachment; filename=example.tif' \
+                -H 'Content-Type: image/tif' \
+                -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
+                -F image=@/Users/geobart/example.tif \
+                -F name=example.tif
+        """
+        
+        if 'image' not in request.data.keys():
             raise ParseError("Empty content")
+        
+        img = request.data['image']
+        name = request.data['name']
 
-        f = request.data['file']
+        # try to open file with rasterio
+        try:
+            with rasterio.open(img) as dataset:
+                data_array = dataset.read() 
+        except:
+            raise ParseError("Unsupported image type opened by Rasterio")
 
-        COG.image.save(f.name, f, save=True)
+        cog = COG.objects.create(name=name, image=img)
+        cog.save()
         return Response(status=status.HTTP_201_CREATED)
+    
+    # def delete(self, request, format=None):
+    #     COG.delete(save=True)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,10 @@ from . import views, api_views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^api/cogs', api_views.COG_APIView.as_view()),
+    url(
+        r'^api/cogs',
+        api_views.COG_APIView.as_view()
+    ),
     url(r'^health', views.health, name='health'),
     url(r'^404', views.handler404, name='404'),
     url(r'^500', views.handler500, name='500'),
